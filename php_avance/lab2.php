2993a22e7eac796c1c1d934d71419dd5434dce3e
jeroso94 <jeroso94@gmail.com>
2022-11-14 15:02:31 +0100

    Classe etudiant v2 - Attribut static

--- php_avance/lab2.php
+++ php_avance/lab2.php
@@ -5,6 +5,14 @@ class Etudiant
     public $nom;
     public $age;
 
+    public static $nbEtudiant;
+    private static $nbMax=20;  //attribut de la classe et non d'instance
+
+    public function __construct()  // à chaque création d'une instance, on incrémente l'attribut static de la classe
+    {
+        self::$nbEtudiant++;
+    }
+
     public function getNom()
     {
         return $this->nom;
@@ -22,12 +30,22 @@ class Etudiant
 
     public function setAge($age)
     {
-        $this->age = $age;     
+        if ($age>18) {
+            $this->age = $age;
+        } else {
+            echo "Valeur de l age invalide!";
+        } 
     }
 
     public function infoEtudiant()
     {
         return "Nom: ".$this->nom ." Age: ".$this->age;
+    }
+
+
+    public static function getNbMax()  //méthode de classe
+    {
+       return self::$nbMax;
     }
 
 }
@@ -38,6 +56,8 @@ $e1 = new Etudiant();
 $e1->setNom("Johnny");
 $e1->setAge(24);
 
+echo "<br/>Nbre actuel etudiants : ".Etudiant::$nbEtudiant;  //appel de l'atribut static
+
 $e2 = new Etudiant();
 
 $e2->setNom("Louis");
@@ -45,6 +65,10 @@ $e2->setAge(27);
 
 echo $e1->infoEtudiant();
 
-echo"<br/>";
+echo "<br/>Nbre actuel etudiants : ".Etudiant::$nbEtudiant;
 
-echo $e2->infoEtudiant();
+echo "<br/>Nbre max etudiants : ".Etudiant::getNbMax();
+
+// echo"<br/>";
+
+// echo $e2->infoEtudiant();
